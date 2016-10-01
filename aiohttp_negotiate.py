eeb5b2974c165b44f2beb37305f3d04dbd4e6905
Alexander Dutton <alexander.dutton@it.ox.ac.uk>
2016-10-01 11:31:15 +0100

    Refactor to do rDNS on the host we've connected to.
    Modelled on requests-negotiate

--- aiohttp_negotiate.py
+++ aiohttp_negotiate.py
@@ -1,35 +1,56 @@
 import asyncio
+import logging
+import socket
+import ssl
+
 import base64
 from http.client import UNAUTHORIZED
-from urllib.parse import urlparse
 
 import aiohttp
 import gssapi
 import www_authenticate
 
+logger = logging.getLogger(__name__)
+
+
 class NegotiateMixin(object):
     def __init__(self, *,
                  negotiate_client_name=None,
                  negotiate_service_name=None,
+                 negotiate_service='HTTP',
                  **kwargs):
         self.negotiate_client_name = negotiate_client_name
         self.negotiate_service_name = negotiate_service_name
-        self.negotiate_contexts = {}
+        self.negotiate_service = negotiate_service
         super().__init__(**kwargs)
 
-    def get_negotiate_context(self, host):
-        if host not in self.negotiate_contexts:
-            service_name = gssapi.Name(self.negotiate_service_name or \
-                                       'HTTP@{0}'.format(host),
-                                       gssapi.NameType.hostbased_service)
-            if self.negotiate_client_name:
-                creds = gssapi.Credentials(name=gssapi.Name(self.negotiate_client_name),
-                                           usage='initiate')
-            else:
-                creds = None
-            self.negotiate_contexts[host] = gssapi.SecurityContext(name=service_name,
-                                                                   creds=creds)
-        return self.negotiate_contexts[host]
+    def get_hostname(self, response):
+        assert isinstance(response, aiohttp.ClientResponse)
+        assert isinstance(response.connection, aiohttp.connector.Connection)
+        sock = response.connection._transport.get_extra_info('socket')
+        assert isinstance(sock, (ssl.SSLSocket, socket.socket))
+        return socket.gethostbyaddr(sock.getpeername()[0])[0]
+
+    def get_context(self, host):
+        service_name = gssapi.Name(self.negotiate_service_name or '{0}@{1}'.format(self.negotiate_service, host),
+                                   gssapi.NameType.hostbased_service)
+        logger.debug("Service name: {0}".format(service_name))
+        if self.negotiate_client_name:
+            creds = gssapi.Credentials(name=gssapi.Name(self.negotiate_client_name),
+                                       usage='initiate')
+        else:
+            creds = None
+        logger.debug("Credentials: {0}".format(creds))
+        return gssapi.SecurityContext(name=service_name,
+                                      creds=creds)
+
+    def get_challenges(self, response):
+        challenges = {}
+        for k, v in response.headers.items():
+             if k.lower() == 'www-authenticate':
+                 challenges.update(www_authenticate.parse(v))
+        logger.debug('Server challenges: {}'.format(challenges))
+        return challenges
 
     def negotiate_step(self, ctx, in_token=None):
         if in_token:
@@ -41,29 +62,24 @@ class NegotiateMixin(object):
 
     @asyncio.coroutine
     def _request(self, method, url, *, headers=None, **kwargs):
-        host = urlparse(url).hostname
         headers = headers or {}
-        resp = yield from super()._request(method, url, headers=headers, **kwargs)
-        challenges = {}
-        for k, v in resp.headers.items():
-            if k.lower() == 'www-authenticate':
-                challenges.update(www_authenticate.parse(v))
-        if resp.status == UNAUTHORIZED and 'negotiate' in challenges:
-            host = urlparse(resp.url).hostname
-            self.negotiate_contexts.pop(host, None)
-            ctx = self.get_negotiate_context(host)
+        response = yield from super()._request(method, url, headers=headers, **kwargs)
+        challenges = self.get_challenges(response)
+        if response.status == UNAUTHORIZED and 'negotiate' in challenges:
+            host = self.get_hostname(response)
+            ctx = self.get_context(host)
             out_token = self.negotiate_step(ctx)
             while True:
-                resp.close()
+                response.close()
                 if out_token:
                     headers['Authorization'] = 'Negotiate ' + out_token
-                resp = yield from super()._request(method, url, headers=headers, **kwargs)
-                challenges = www_authenticate.parse(resp.headers.get('WWW-Authenticate'))
+                    response = yield from super()._request(method, url, headers=headers, **kwargs)
+                challenges = www_authenticate.parse(response.headers.get('WWW-Authenticate'))
                 in_token = challenges['negotiate']
                 self.negotiate_step(ctx, in_token)
                 if ctx.complete:
                     break
-        return resp
+        return response
 
 class NegotiateClientSession(NegotiateMixin, aiohttp.ClientSession):
     pass
